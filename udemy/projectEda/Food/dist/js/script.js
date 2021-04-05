d8a3976fd78fe65af85c2d2c37c5dac5b32a9944
akartamysh <akartamysh@student.ucode.world>
2021-04-05 17:43:44 +0300

    finish slide#2 + nav_bar

--- udemy/projectEda/Food/dist/js/script.js
+++ udemy/projectEda/Food/dist/js/script.js
@@ -551,6 +551,26 @@ window.addEventListener('DOMContentLoaded', ()=>{ // делаем загрузк
         dots.forEach(dot => dot.style.opacity = '.5')
         dots[slideIndex -1].style.opacity = 1
     });
-    //test
+    
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) =>{
+            const slideTo =e.target.getAttribute('data-slide-to')
+
+            slideIndex = slideTo
+            offset = +width.slice(0, width.length -2) * (slideTo -1)
+
+            slidesField.style.transform = `translateX(-${offset}px)`
+
+            if (slides.length < 10) {
+                current.textContent =  `0${slideIndex}`;
+            } else {
+                current.textContent =  slideIndex;
+            }
+
+            dots.forEach(dot => dot.style.opacity = '.5')
+            dots[slideIndex -1].style.opacity = 1
+        })
+    })
     
 });
