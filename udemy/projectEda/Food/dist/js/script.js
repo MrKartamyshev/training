df83eb1656d2a11d13fda28d13599fc706cf12db
akartamysh <akartamysh@student.ucode.world>
2021-04-05 17:09:04 +0300

    add naw_barfor slider

--- udemy/projectEda/Food/dist/js/script.js
+++ udemy/projectEda/Food/dist/js/script.js
@@ -423,6 +423,7 @@ window.addEventListener('DOMContentLoaded', ()=>{ // делаем загрузк
     let slideIndex = 1;
 
     const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slide'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
@@ -430,6 +431,8 @@ window.addEventListener('DOMContentLoaded', ()=>{ // делаем загрузк
         slidesWrapper = document.querySelector('.offer__slider-wrapper'),
         width = window.getComputedStyle(slidesWrapper).width,
         slidesField = document.querySelector('.offer__slider-inner');
+        
+
 
     if (slides.length < 10) {
         total.textContent = `0${slides.length}`;
@@ -448,6 +451,54 @@ window.addEventListener('DOMContentLoaded', ()=>{ // делаем загрузк
     slides.forEach(slide => {
         slide.style.width = width;
     });
+
+    slider.style.position = 'relative'
+
+
+    // создаем индекатор и добавляем на страничку
+    const indicators = document.createElement('ol')
+    indicators.classList.add('carousel-indicators')
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;    
+    `;
+
+    slider.append(indicators)
+
+    for (let i = 0; i < slides.length; i++){
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1)// устанавливаем атрибут / каждой точке устанавливаем дата слайд и нумерацию 
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `
+        if (i == 0){
+            dot.style.opacity = 1
+        }
+        indicators.append(dot)
+        dots.pusd(dot)
+    }
+
+
 
     next.addEventListener('click', () => {
         if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
@@ -469,6 +520,9 @@ window.addEventListener('DOMContentLoaded', ()=>{ // делаем загрузк
         } else {
             current.textContent =  slideIndex;
         }
+
+        dots.forEach(dot => dot.style.opacity = '.5')
+        dots[slideIndex -1].style.opacity = 1
     });
 
     prev.addEventListener('click', () => {
@@ -491,5 +545,9 @@ window.addEventListener('DOMContentLoaded', ()=>{ // делаем загрузк
         } else {
             current.textContent =  slideIndex;
         }
+
+
+        dots.forEach(dot => dot.style.opacity = '.5')
+        dots[slideIndex -1].style.opacity = 1
     });
 });
