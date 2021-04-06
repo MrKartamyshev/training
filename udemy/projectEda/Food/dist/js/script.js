cca65a79db67e554fbcf9a68187d2a23ce2e2c0f
akartamysh <akartamysh@student.ucode.world>
2021-04-06 18:17:55 +0300

    Добавил в калькулятор сохранение в локал сторедж что бы при перезагрузки сохранялись данные

--- udemy/projectEda/Food/dist/js/script.js
+++ udemy/projectEda/Food/dist/js/script.js
@@ -576,9 +576,27 @@ window.addEventListener('DOMContentLoaded', ()=>{ // делаем загрузк
 
     //calc
         const result = document.querySelector('.calculating__result span');
-        let sex = 'female',
-            height, weight, age,
-            ratio = 1.375;
+
+        let sex,height, weight, age, ratio;
+
+
+        // записывем в локалсторедж
+        if (localStorage.getItem('sex')){
+            sex = localStorage.getItem('sex')
+        } else {
+            sex = 'female'
+            localStorage.getItem('sex', 'female')
+        }
+        
+        if (localStorage.getItem('ratio')){
+            ratio = localStorage.getItem('ratio')
+        } else {
+            ratio = 1.375
+            localStorage.getItem('ratio', 1.375)
+        }
+
+
+
 
         function calcTotal() {
             if (!sex || !height || !weight || !age || !ratio) {
@@ -594,15 +612,36 @@ window.addEventListener('DOMContentLoaded', ()=>{ // делаем загрузк
 
         calcTotal();
 
-        function getStaticInformation(parentSelector, activeClass) {
-            const elements = document.querySelectorAll(`${parentSelector} div`);
+        function initLocalSetting(selector, activeClass){
+            const element = document.querySelectorAll(selector)
+
+            element.forEach(elem => {
+                elem.classList.remove(activeClass)
+                if (elem.getAttribute('id') === localStorage.getItem('sex')){
+                    elem.classList.add(activeClass)
+                }
+                if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')){
+                    elem.classList.add(activeClass)
+                }
+            })
+        }
+       
+
+        initLocalSetting('#gender div', 'calculating__choose-item_active')
+        initLocalSetting('.calculating__choose_big div', 'calculating__choose-item_active')
+
+
+        function getStaticInformation(selector, activeClass) {
+            const elements = document.querySelectorAll(selector);
 
             elements.forEach(elem => {
                 elem.addEventListener('click', (e) => {
                     if (e.target.getAttribute('data-ratio')) {
                         ratio = +e.target.getAttribute('data-ratio');
+                        localStorage.setItem('ratio', e.target.getAttribute('data-ratio'))
                     } else {
                         sex = e.target.getAttribute('id');
+                        localStorage.setItem('sex',e.target.getAttribute('id'))
                     }
         
                     elements.forEach(elem => {
@@ -616,13 +655,20 @@ window.addEventListener('DOMContentLoaded', ()=>{ // делаем загрузк
             });
         }
 
-        getStaticInformation('#gender', 'calculating__choose-item_active');
-        getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+        getStaticInformation('#gender div', 'calculating__choose-item_active');
+        getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
 
         function getDynamicInformation(selector) {
             const input = document.querySelector(selector);
 
             input.addEventListener('input', () => {
+
+                if (input.value.match(/\D/g)){// когда вводим не чесло будем подсвечивать импут 
+                    input.style.border = '1px solid red'
+                } else {
+                    input.style.border = 'none'
+                }
+
                 switch(input.getAttribute('id')) {
                     case "height":
                         height = +input.value;
